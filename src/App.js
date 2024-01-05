2077b14a89e679b45b0f9e99cd855b042b17cd23
Saransh Chauhan <k.saranshchauhan@gmail.com>
2024-01-05 16:32:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import StockPrice from './components/StockPrice'
+
+
+function App() {
+  return (
+    <div>
+      <StockPrice/>
+    </div>
+  )
+}
+
+export default App
